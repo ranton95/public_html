52bcb1acf9f705e2717d5ad53436d1fb33c9f5a2
ranton95 <royceanton1995@gmail.com>
2025-09-29 10:32:03 +0200

    validate date function add

--- week1_290925/validate_date.php
+++ week1_290925/validate_date.php
@@ -9,18 +9,22 @@
 
 <?php 
 //validate.php
+// test at: http://localhost/~joseroyce/
 
 $date = "29.02.2025";
 
 
-function isValidDate($date, $format = "Y-m-d"): bool{
+function isValidDate(string $date, $format = "Y-m-d"): bool{
     $date_array = explode(".", $date);
+    echo "<pre>";
     print($date_array);
+    echo "</pre>";
+    return true;
     // $d= DateTime::createFromFormat($format, $date);
     // return $d && $d
 }
 
-if( is)
+isValidDate($date)
 
 ?>
 
