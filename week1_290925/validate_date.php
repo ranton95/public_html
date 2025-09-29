e9c8f1d9c1d83421dc77f58e1bb1f5cf00c564a1
ranton95 <royceanton1995@gmail.com>
2025-09-29 10:28:09 +0200

    Week 1: php date validate

---
+++ week1_290925/validate_date.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+//validate.php
+
+$date = "29.02.2025";
+
+
+function isValidDate($date, $format = "Y-m-d"): bool{
+    $date_array = explode(".", $date);
+    print($date_array);
+    // $d= DateTime::createFromFormat($format, $date);
+    // return $d && $d
+}
+
+if( is)
+
+?>
+
+</body>
+</html>
