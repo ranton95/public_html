7aff2c4f541975e4c7d94cc4b9c2e344a90e8893
ranton95 <68892523+ranton95@users.noreply.github.com>
2025-11-18 12:48:26 +0000

    file read

--- week4_181125/file_read.php
+++ week4_181125/file_read.php
@@ -6,13 +6,26 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    
-    $file = "person.csv";
-    if(!file_exists($file)){
-        die("File $file does not exist or not found!");
-    }
-    
-    ?>
+<?php
+
+$file = "person.csv";
+if(!file_exists($file)){
+    die("File $file does not exist or not found!");
+}
+
+$fp = fopen($file, "r");
+
+if(!$fp){
+    die('Error while opening file!');
+}
+
+
+$header_array = fgetcsv($fp);
+// $header_array = explode(",", $header);
+echo "<prev>";
+print_r($header_array);
+echo "</prev>";
+
+?>
 </body>
 </html>
