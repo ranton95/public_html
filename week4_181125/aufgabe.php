51d068a79eba791c95b22706f089f91bd27f5380
Royce <royceanton1995@gmail.com>
2025-11-19 13:18:02 +0100

    aufgabe

---
+++ week4_181125/aufgabe.php
@@ -0,0 +1,18 @@
+
+/*
+
+Erstellen Sie ein PHP-Skript namens „log_access.php”, das jeden Aufruf der Seite in
+der Textdatei „access_log.txt” speichert.
+Beim Aufruf der Seite soll das Skript das aktuelle Datum und die aktuelle Uhrzeit
+erfassen.
+- das aktuelle Datum und die aktuelle Uhrzeit erfassen.
+- die IP-Adresse des Clients ermitteln (Tipp: $_SERVER)
+- diese Informationen in die Datei access_log.txt im selben Verzeichnis
+schreiben.
+Bonus:
+Geben Sie im Browser zusätzlich an, wie viele Zugriffe insgesamt schon
+protokolliert wurden.
+Tipp: Schauen Sie sich die Funktionen file() und readfile() an oder nutzen Sie
+fread() wie bisher.
+
+*/
