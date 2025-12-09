e86d2bc0cd8fe92b530d54ba653b15ebd916e4cb
Royce <royceanton1995@gmail.com>
2025-12-09 13:54:42 +0100

    local changes

---
+++ week5_091225/pdo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+    
+    $pdo = new PDO("mysql:host=localhost;dbname=testdb", "root", "");
+    echo "Verbindung erfolgreich!";
+
+    $sql = "CREATE TABLE person (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            first_name VARCHAR(50) NOT NULL,
+            last_name VARCHAR(100) NOT NULL
+            )";
+
+    $sql = "INSERT INTO person VALUES (
+            'Gustav'
+            'Peter'
+        );
+    ";
+    
+    $pdo->exec($sql);
+
+    ?>php
+
+</body>
+</html>
