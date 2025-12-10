eafafd0ee409fa401b0512ae449c3b8b55ef70f5
Royce <royceanton1995@gmail.com>
2025-12-10 13:51:34 +0100

    learn: creatinga a connection to a php my admin server

---
+++ week5_091225/wednesday/db_connection_data.php
@@ -0,0 +1,13 @@
+<?php 
+
+// db connection data.php
+
+$db_engine = "mysql";
+$db_name = "E1FI2_2024_Sta";
+$db_host = "localhost";
+$db_user = "phpmyadmin";
+$db_password = "server";
+
+
+
+?>
---
+++ week5_091225/wednesday/db_connection_function.php
@@ -0,0 +1,27 @@
+<?php 
+
+// db connection function.php
+
+function db_connect(){
+    require_once "db_connection_data.php";
+    $db_server = "$db_engine:dbname=$db_name;host=$db_host";
+
+    try{
+        $pdo = new PDO($db_server, $db_user, $db_password);
+        echo "<p>DB connection successfuly established </p>";
+        return $pdo;
+    }
+    catch(PDOException $e){
+        echo $e->getMessage();
+        die("<p>Could not connect to DB! Try again later</p>");
+    }
+    
+}
+
+db_connect();
+
+?>
+
+// after this go to http://localhost/phpmyadmin/ and login with user: phpmyadmin 
+// password: server and create a database table: E1FI2_2024_Sta as mentioned in the db_connecton_data file
+
