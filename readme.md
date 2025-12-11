344303f0bd3a2f35024e73f235b148cf65671a49
ranton95 <68892523+ranton95@users.noreply.github.com>
2025-12-11 09:03:26 +0000

    learn-stk: creating an xml file through php

---
+++ readme.md
@@ -0,0 +1,13 @@
+
+
+In the terminal, run your PHP file directly:
+This will execute your script and create the personen.xml file.
+```
+php xmlerzeugen.php
+```
+
+If you want to start a PHP development server to serve your files over HTTP, run:
+```
+php -S localhost:8000
+```
+Then you can access your files at http://localhost:8000 in the browser.
---
+++ week5_091225/thursday-schattke/xmlerzeugen.php
@@ -0,0 +1,27 @@
+<?php
+
+$xmlDoc = new DOMDocument("1.0", "utf-8");
+$xmlDoc->formatOutput = true;
+
+
+//Root Element 
+$xmlRoot = $xmlDoc->createElement("personen");
+$xmlDoc->appendChild($xmlRoot);
+
+// 1st node user
+$xmlPerson = $xmlDoc->createElement("benutzer"); 
+
+// child node of user: username and password
+$xmlBenutzername = $xmlDoc->createElement("benutzer", "schattke");
+$xmlPerson->appendChild($xmlBenutzername);
+
+$xmlPasswort = $xmlDoc->createElement("passwort", "12345");
+$xmlPerson->appendChild($xmlPasswort);
+
+$xmlRoot->appendChild($xmlPerson);
+
+//create file
+$file = "personen.xml";
+$xmlDoc->save($file);
+
+?>
