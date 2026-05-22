94f3772c8f734d4d5d4fc5c98ae92ed51d36889e
ranton95 <royceanton1995@gmail.com>
2026-05-22 12:09:13 +0200

    stk: chore backend

--- 26-05-MAI/stk/task/get_data.php
+++ 26-05-MAI/stk/task/get_data.php
@@ -1,20 +1,22 @@
 <?php
-// Set headers to allow JSON output
+//1.Set headers to allow JSON output
 header('Content-Type: application/json');
 
+//2.Endpoint parameter input, provided or empty
 $qualifikation = $_GET['qualifikation'] ?? "";
 
+//3.connection to phpmyadmin server
 $host = 'localhost'; 
-$username = 'phpmyadmin'; // Your MySQL username
+$username = 'phpmyadmin';
 $password = 'server';    
 $dbname = 'json'; 
 
 try {
-    // Establish connection to the MySQL database
+    //4.Establish connection to the MySQL database
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
+    //5.Execute query
     $sql = "SELECT mitarbeiter.Name as name,
                    qualifikation.Qualifikation as qualification 
             FROM mitarbeiter
@@ -33,7 +35,7 @@ try {
     echo json_encode($results);
 
 } catch (PDOException $e) {
-    // Return an error message if connection fails
+    //6. Return an error message if connection fails
     http_response_code(500);
     echo json_encode(["error" => "Database connection failed: " . $e->getMessage()]);
 }
